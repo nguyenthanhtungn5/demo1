9ea9d6b3764e94b1d1ad03342bec0787cf607fc6
OEM Configuration (temporary user) <nguyenthanhtungn5@gmail.com>
2020-04-29 23:02:34 +0200

    encrypted Id

--- demo1/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ demo1/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -7,19 +7,26 @@ import org.springframework.stereotype.Service;
 import com.example.demo.UserRepository;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.service.UserService;
+import com.example.demo.shared.Utils;
 import com.example.demo.shared.dto.UserDto;
 @Service
 public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
+	@Autowired
+	Utils utils;
 	@Override
 	public UserDto createUser(UserDto user) {
 				
 		if(userRepository.findByEmail(user.getEmail()) !=null) throw new RuntimeException("Record already exits") ;
 		UserEntity userEntity=new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
+		
+		String publicUserId=utils.generateUserId(30);
+		
+		
 		userEntity.setEncytedPassword("test");
-		userEntity.setUserId("testUserId");
+		userEntity.setUserId(publicUserId);
 		UserEntity storedUserDetails=userRepository.save(userEntity);
 		
 		UserDto returnValue= new UserDto();
